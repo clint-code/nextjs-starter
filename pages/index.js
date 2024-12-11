253efa75bed6a1dbf367c06a0b7c3ffe0cc35d6c
Clinton Mutinda <cmutinda@mfs.co.ke>
2024-12-11 11:30:26 +0300

    Metadata and Assets

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className={styles.container}>
-
+      
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,31 @@
 import Link from "next/link";
+import Image from "next/image";
+import Head  from "next/head";
+import Script from "next/script";
 
 export default function FirstPost(){
     return (
         <>
+            <Head>
+                <title>First Post</title>
+            </Head>
+
+            {/* <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() =>
+                    console.log(`script loaded correctly, window.FB has been populated`)
+                }
+            /> */}
+
             <h1>First Post</h1>
+
+            <Image
+                src="/images/f1-logo.png"
+                height={200}
+                width={400}
+                alt="F1-Logo"
+            />
 
             <h2>
                 <Link href="/">Back to home</Link>
